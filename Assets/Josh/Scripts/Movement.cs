838f757fc98a87052f01688c779a295fe3a14a48
Joshua Jopling <149619818+Joshua-2006@users.noreply.github.com>
2025-02-19 09:01:52 -0800

    The Player Can Move and Rotate

---
+++ Assets/Josh/Scripts/Movement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float turnspeed = 200f;
+    public float thrust = 5f;
+    [SerializeField] public float drag = 0.99f;
+    private float horizontal;
+    [SerializeField] private float vertical;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        float rotationAmount = horizontal * turnspeed * Time.deltaTime;
+        transform.Rotate(0f, 0f, -rotationAmount);
+
+        rb.AddForce(transform.up * vertical * thrust);
+        rb.velocity *= drag;
+    }
+}
