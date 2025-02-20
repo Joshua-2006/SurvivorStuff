b7d3aceda6cd8dc68020724311d284e69a8ca5b3
Joshua Jopling <149619818+Joshua-2006@users.noreply.github.com>
2025-02-20 09:51:45 -0800

    Enemy follows you

--- Assets/Josh/Scripts/Enemy.cs
+++ Assets/Josh/Scripts/Enemy.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public GameObject player;
-
+    public Movement player;
+    public float speed;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = FindAnyObjectByType<Movement>();
     }
 
     // Update is called once per frame
     void Update()
     {
-           
+        if(player != null)
+        {
+            Vector3 direction = (player.transform.position - transform.position).normalized;
+            transform.position += direction * speed * Time.deltaTime;
+        }
     }
 }
--- Assets/Josh/Scripts/SpawnManager.cs
+++ Assets/Josh/Scripts/SpawnManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField] private int enemies;
+    [SerializeField] private Enemy enemy;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,22 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        InvokeRepeating("Spawn", 1, 5);
+    }
+    public Vector3 SpawnPos()
+    {
+        float xRange = Random.Range(-10, 10);
+        float yRange = Random.Range(-10, 10);
+        Vector3 randomPos = new Vector3(xRange, yRange, 0);
+        return randomPos;
+    }
+    public void Spawn()
+    {
+        Instantiate(enemy, SpawnPos(), enemy.transform.rotation);
+    }
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(5);
+        Spawn();
     }
 }
