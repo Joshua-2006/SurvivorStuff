6d76fd4d626ba47e3c45b603d621ce1dc17d8000
ThyCoolPerson <jradchishin@gmail.com>
2025-02-21 08:43:13 -0800

    Update Movement.cs

--- Assets/Josh/Scripts/Movement.cs
+++ Assets/Josh/Scripts/Movement.cs
@@ -15,6 +15,8 @@ public class Movement : MonoBehaviour
     public bool hasPowerup;
     public GameObject cam;
     public Vector3 offset = new Vector3(0, 0, -10);
+    private float powerupStrength = 15.0f;
+    public GameObject powerupIndicator;
     // Start is called before the first frame update
     void Start()
     {
@@ -41,6 +43,24 @@ public class Movement : MonoBehaviour
         {
             hasPowerup = true;
             Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+
+            IEnumerator PowerupCountdownRoutine()
+            {
+                yield return new WaitForSeconds(7);
+                hasPowerup = false;
+            }
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+        Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+            
+            Debug.Log("Collided with " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+        enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
     }
 }
